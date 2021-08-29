ac41fc1736c70b68da7eb3ad4764d5d1aa03be12
SAwarm <jonasdasilvaelias@gmail.com>
2021-08-28 23:20:29 -0300

    alteracoesArquivoNotasMaioresMenores

--- MaiorMenorNotas.py
+++ MaiorMenorNotas.py
@@ -2,11 +2,16 @@ print("Para sair do programa, digite uma nota fora do intervalo de 0.0 à 10.0")
 notas = []
 nota = 0
 x = 0
+contador = 0
 while(x == 0):
     nota = float(input("Digite a nota do aluno: "))
     if(nota <= 10 and nota >= 0):
-        notas.append(nota)
+        notas.append(float(nota))
     else:
         x = 1
-print(min(notas))
-print(max(notas))
+    contador = contador + 1
+if(contador > 2):
+    print("Menor nota: ", min(notas))
+    print("Maior nota: ", max(notas))
+else:
+    print("Digite no mínimo duas notas válidas")
