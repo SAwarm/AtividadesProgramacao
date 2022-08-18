53c9dd594b4745da9419dfd5dbce43cf96e1051f
jonas-elias <jonasdasilvaelias@gmail.com>
2022-08-17 22:12:34 -0300

    fix: comments in methods

--- HigherLess/HigherLessNotes.py
+++ HigherLess/HigherLessNotes.py
@@ -1,7 +1,17 @@
+'''
+    Method to show warning
+
+    @return void
+'''
 def initialWarning():
     print("Quit the program, input a number outside the interval of 0.0 to 10.0")
 
 
+'''
+    Method to received notes of user
+
+    @return: array
+'''
 def inputNote():
     cont = 0
     notes = []
@@ -24,23 +34,45 @@ def inputNote():
     ]
 
 
-def reportNotes(cont, notes, minNotes):
-    if (cont >= minNotes):
+'''
+    Method to show report
+
+    @param totalNotes: int
+    @param notes: array
+    @param minNotes: int
+    @return: void
+'''
+def reportNotes(totalNotes, notes, minNotes):
+    if (totalNotes >= minNotes):
         HigherNote(notes)
         LowerNote(notes)
         return
     print("The min number notes is: ", minNotes)
 
 
+'''
+    Method to show Higher note
+
+    @param notes: array
+    @return: void
+'''
 def HigherNote(notes):
     print("Higher note: ", max(notes))
 
 
+'''
+    Method to show lower note
+
+    @param notes: array
+    @return: void
+'''
 def LowerNote(notes):
     print("Lower note: ", min(notes))
 
 
+# Initial warning
 initialWarning()
 
+# Input notes array and show Higher and Lower note
 response = inputNote()
 reportNotes(response[0]["cont"], response[0]["notes"], 2)
